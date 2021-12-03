8603e78fcb4c295782561a61bddc1bb40bb59dee
Elijahdanie <elijahdanie@gmail.com>
2021-12-03 08:48:42 +0100

    betty compliant

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,14 +9,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *bt_node;
+binary_tree_t *bt_node;
 
-    bt_node = malloc(sizeof(binary_tree_t));
-    if (bt_node == NULL)
-        return (NULL);
-    bt_node->n = value;
-    bt_node->parent = parent;
-    bt_node->left = NULL;
-    bt_node->right = NULL;
-    return (bt_node);
+bt_node = malloc(sizeof(binary_tree_t));
+if (bt_node == NULL)
+return (NULL);
+bt_node->n = value;
+bt_node->parent = parent;
+bt_node->left = NULL;
+bt_node->right = NULL;
+return (bt_node);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,27 +9,27 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *bt_node, *leftnode_cache;
+binary_tree_t *bt_node, *leftnode_cache;
 
-    bt_node = malloc(sizeof(binary_tree_t));
-    if (!bt_node)
-    {
-        return NULL;
-    }
-    bt_node->n = value;
-    bt_node->left = NULL;
-    bt_node->right = NULL;
-    if (!parent->left)
-    {
-        bt_node->left = parent->left;
-        bt_node->left->parent = bt_node;
-        bt_node->parent = parent;
-        parent->left = bt_node;
-    }
-    else
-    {
-        bt_node->parent = parent;
-        parent->left = bt_node;
-    }
-    return bt_node;
+bt_node = malloc(sizeof(binary_tree_t));
+if (!bt_node)
+{
+return NULL;
+}
+bt_node->n = value;
+bt_node->left = NULL;
+bt_node->right = NULL;
+if (!parent->left)
+{
+bt_node->left = parent->left;
+bt_node->left->parent = bt_node;
+bt_node->parent = parent;
+parent->left = bt_node;
+}
+else
+{
+bt_node->parent = parent;
+parent->left = bt_node;
+}
+return bt_node;
 }
